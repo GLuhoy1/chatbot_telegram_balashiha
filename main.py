bc535b744afb0cf776eede40e97b3d81bb970e3d
GLuhoy1 <s.lopatin96@gmail.com>
2024-01-24 16:30:27 +0300

    Добавленн пример файла конфиденциальных данных, изменён мэйн в соответствии с контейненром

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from confidential import Confidential as Con_data
 from DialogTree import DialogTree
 from BaseFunc import BaseFunc
 from EmailHandler import EmailHandler
+import time
+# from telegram import User
 
 # Определение константы для текста "В начало"
 START_TEXT = "В начало"
@@ -19,11 +21,16 @@ class BotLogic(BaseFunc):
     def send_start_message(self, chat_id):
         self.chat_states[chat_id] = {'current_state': self.start_menu_btn(DialogTree.telegram_bot_data)}
         keyboard = self.create_keyboard(self.chat_states[chat_id]['current_state'])
-        self.send_message(chat_id, "Добро пожаловать в бота!", reply_markup=keyboard)
+        self.send_message(chat_id, "Добро пожаловать в справочного телеграмм бота! "
+                                   "На данный момент он находится на этапе тестирования.", reply_markup=keyboard)
 
     def handle_custom_message_email(self, message, to_email):
         chat_id = message.chat.id
         text = message.text
+
+        # Получаем данные о пользователе
+        user = message.from_user
+        user_info = f"Имя: {user.first_name}\nФамилия: {user.last_name}\nUsername: {user.username}\nChat ID: {chat_id}"
 
         if chat_id not in self.chat_states:
             self.send_start_message(chat_id)
@@ -33,8 +40,8 @@ class BotLogic(BaseFunc):
             # Пользователь только что выбрал отправку письма, просим описать вопрос
             self.chat_states[chat_id]['waiting_for_contact_info'] = True
             self.chat_states[chat_id]['custom_message'] = text
-            self.send_message(chat_id, "Пожалуйста, не забудьте оставить ваши контактные данные "
-                                       "(номер телефона или email):")
+            self.send_message(chat_id, "Коротко опишите ваш вопрос и не забудьте оставить ваши контактные данные "
+                                       "(номер телефона в формате +79009009000 или email):")
         else:
             # Создаем экземпляр EmailHandler
             smtp_server = Con_data.smtp_server
@@ -46,8 +53,9 @@ class BotLogic(BaseFunc):
             email_handler = EmailHandler(smtp_server, smtp_port, smtp_username, smtp_password, from_email)
 
             # Отправляем сообщение с контактами через EmailHandler
-            custom_message = self.chat_states[chat_id]['custom_message']
-            if EmailHandler.has_contact_info(text):  # Проверяем наличие контактных данных
+            custom_message = f"{self.chat_states[chat_id]['custom_message']}\n\nUser Info:\n{user_info}"
+
+            if EmailHandler.has_contact_info(text):
                 if email_handler.send_email(to_email, "Вопрос от пользователя", custom_message, text):
                     self.send_message(chat_id, "Ваш вопрос отправлен. Спасибо!")
 
@@ -57,7 +65,7 @@ class BotLogic(BaseFunc):
                 else:
                     self.send_message(chat_id, "Произошла ошибка при отправке вопроса. Попробуйте позже.")
             else:
-                self.send_message(chat_id, "Пожалуйста, укажите контактные данные (номер телефона или email) "
+                self.send_message(chat_id, "Пожалуйста, укажите ваши контактные данные (номер телефона или email) "
                                            "для отправки вашего вопроса.")
 
     def handle_message(self, message):
@@ -80,7 +88,7 @@ class BotLogic(BaseFunc):
         elif current_state == ["Написать письмо в ВУЗ"]:
             to_email = Con_data.vuz_email
             self.handle_custom_message_email(message, to_email)
-        elif "Отправить письмо в приёмную комиссию" in current_state:
+        elif "Написать вопрос в приёмную комиссию" in current_state:
             to_email = Con_data.priemka_email
             self.handle_custom_message_email(message, to_email)
         else:
@@ -113,5 +121,12 @@ class BotLogic(BaseFunc):
 
 if __name__ == "__main__":
     token_value = Con_data.token_value
-    my_bot = BotLogic(token_value)
-    my_bot.run()
+
+    while True:
+        try:
+            my_bot = BotLogic(token_value)
+            my_bot.run()
+        except Exception as e:
+            print(f"Произошла ошибка: {str(e)}")
+            print("Перезапуск через 2 секунды...")
+            time.sleep(2)
